389dedf554ea6ca03a70447e52b598310c6b08fd
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-08-18 22:24:49 -0300

    teste primeira commitao

---
+++ Assets/ProjetoMedo/TesteOnda.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteOnda : MonoBehaviour
+{
+    public GameObject onda;
+
+    public Vector3 scaleOndaI = new Vector3(1f, 1f, 1f);
+    public Vector3 scaleOndaF = new Vector3(10f, 10f, 10f);
+
+    public float scaleVelo = 2f;
+
+    void Update()
+    {
+        Vector3 pposition = transform.position;
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject newOnda = Instantiate(onda, pposition, Quaternion.identity);
+
+            newOnda.transform.localScale = scaleOndaI;
+
+            StartCoroutine(CrescerAteMorrer(newOnda));
+        }
+    }
+
+    IEnumerator CrescerAteMorrer(GameObject objeto)
+    {
+        float tempo = 0f;
+
+        while (tempo<scaleVelo)
+        {
+            tempo += Time.deltaTime;
+
+            objeto.transform.localScale = Vector3.Lerp(scaleOndaI, scaleOndaF, tempo / scaleVelo);
+
+            yield return null;
+        }
+        Destroy (objeto);
+    }
+}
