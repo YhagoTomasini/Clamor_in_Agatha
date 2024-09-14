2c2e08c79e0f99989b38a5a071a580fc2df6694e
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-09-14 19:10:11 -0300

    cod buff

---
+++ Assets/ProjetoMedo/Scripts/Buffs.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buffs : MonoBehaviour
+{
+    public GameObject orbsBuff;
+    public GameObject barraProgressao;
+    public float valorProgressao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        valorProgressao = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //if Colidir com tag de buff (other == CompareTag("Buffs"))
+        {
+            //Aumentar barra de progressao. Por animação bm estilo flowlab.
+            valorProgressao += 1;
+
+            Destroy(gameObject);
+        }
+
+        if (valorProgressao >= 2)
+        {
+            //Aumentar barra de progressao.
+            //Ativar buff de quebrar.
+
+            //Se colidir com objeto invisivel de caixa;
+                //ativar objeto de aviso de ativacao
+                //Se apertar botao
+                    //funcao da caixa               
+        }
+        if (valorProgressao >= 5)
+        {
+            //Aumentar barra de progressao.
+            //Ativar buff de pular.
+
+            //Se colidir com objeto invisivel de altonivel;
+                //ativar objeto de aviso de ativacao
+                //Se apertar botao
+                    //funcao de pulo  
+
+        }
+        if (valorProgressao >= 9)
+        {
+            //Aumentar barra de progressao.
+            //Ativar buff de shrek.
+
+            //Se colidir com objeto invisivel de shirink;
+                //ativar objeto de aviso de ativacao
+                //Se apertar botao
+                    //funcao de shrink  
+
+        }
+    }
+}
