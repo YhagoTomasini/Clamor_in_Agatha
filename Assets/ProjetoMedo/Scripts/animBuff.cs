56c27f4942767c45c851aed678673c4c815b5ec3
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2024-09-16 22:46:56 -0300

    buff atu

--- Assets/ProjetoMedo/Scripts/animBuff.cs
+++ Assets/ProjetoMedo/Scripts/animBuff.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class animBuff : MonoBehaviour
 {
-    public float amplitude = 0.5f;
+    public float amplitude = 0.1f;
     public float velocidade = 1f;
+    public float escalaAmplitude = 0.1f;
+    public float escalaVelocidade = 1f;
+
     private Vector3 posicaoInicial;
+    private Vector3 escalaInicial;
 
     void Start()
     {
         posicaoInicial = transform.position;
+        escalaInicial = transform.localScale;
     }
 
     void Update()
     {
         Vector3 novaPosicao = posicaoInicial;
         novaPosicao.y += Mathf.Sin(Time.time * velocidade) * amplitude;
-
         transform.position = novaPosicao;
+
+        float escalaFator = 1 + Mathf.Sin(Time.time * escalaVelocidade) * escalaAmplitude;
+        transform.localScale = escalaInicial * escalaFator;
     }
 }
