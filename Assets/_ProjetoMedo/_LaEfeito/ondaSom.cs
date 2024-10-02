f9f80efd62a714a64c649528ccb91e1134c2215c
YhagoTomasini <yhagooliveira20@gmail.com>
2024-10-02 15:03:42 -0300

    death no game

--- Assets/_ProjetoMedo/_LaEfeito/ondaSom.cs
+++ Assets/_ProjetoMedo/_LaEfeito/ondaSom.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TesteOnda : MonoBehaviour
+public class ondaSom : MonoBehaviour
 {
     public GameObject onda;
 
