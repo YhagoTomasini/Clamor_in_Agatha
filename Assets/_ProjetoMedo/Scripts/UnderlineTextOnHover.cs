ec6df833a157502964426c56f02f8e4fa28266fd
JPFurlanis <148451182+JPFurlanis@users.noreply.github.com>
2024-10-28 16:49:27 -0300

    Ui update som

--- Assets/_ProjetoMedo/Scripts/UnderlineTextOnHover.cs
+++ Assets/_ProjetoMedo/Scripts/UnderlineTextOnHover.cs
@@ -6,11 +6,13 @@ public class UnderlineTextOnHover : MonoBehaviour, IPointerEnterHandler, IPointe
 {
     // Referência para o TextMeshProUGUI
     private TextMeshProUGUI textComponent;
+    private AudioSource audioSource;
 
     void Start()
     {
         // Obtém o componente TextMeshProUGUI do objeto
         textComponent = GetComponent<TextMeshProUGUI>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Evento disparado quando o ponteiro do mouse entra na área do texto
@@ -18,6 +20,11 @@ public class UnderlineTextOnHover : MonoBehaviour, IPointerEnterHandler, IPointe
     {
         // Adiciona a tag de sublinhado ao texto
         textComponent.text = "<u>" + textComponent.text + "</u>";
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+
     }
 
     // Evento disparado quando o ponteiro do mouse sai da área do texto
@@ -25,5 +32,9 @@ public class UnderlineTextOnHover : MonoBehaviour, IPointerEnterHandler, IPointe
     {
         // Remove a tag de sublinhado
         textComponent.text = textComponent.text.Replace("<u>", "").Replace("</u>", "");
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
     }
 }
